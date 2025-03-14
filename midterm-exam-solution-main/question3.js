a00569b5b6f97f9f4722aed9bcb4b2ac255b5564
NELMIDA_crt <148179953+CRT1223@users.noreply.github.com>
2025-03-14 04:19:04 -0700

    Update question3.js

--- midterm-exam-solution-main/question3.js
+++ midterm-exam-solution-main/question3.js
@@ -7,6 +7,7 @@ app.get('/test', (req, res) => {
     res.json({ message: 'Express is working! Peter Luigi M. Nelmida' });
 });
 
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
