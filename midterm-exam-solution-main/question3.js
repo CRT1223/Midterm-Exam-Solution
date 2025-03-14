d950fcf212ed938ecf2b5d43a3b994fbd3ad5b4c
unknown <TT229@ccs.local>
2025-03-14 03:52:49 -0700

    Initial commit

---
+++ midterm-exam-solution-main/question3.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Route to test the server
+app.get('/test', (req, res) => {
+    res.json({ message: 'Express is working! Peter Luigi M. Nelmida' });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
